03895d8abfe0875049240556da70285de7c25737
ray <ray81415@gmail.com>
2022-09-02 18:31:00 +0800

    [feat] forge deploy script

---
+++ forge-deploy.sh
@@ -0,0 +1,14 @@
+source .env
+
+IFS="/"
+read -a WORDS <<< $1
+IFS=""
+
+rm -r ${PROJECT_ROOT}/broadcast/${WORDS[-1]}
+
+forge script ${1}:${2} \
+--rpc-url ${ETH_RPC_URL} \
+--private-key ${PRIVATE_KEY} \
+--etherscan-api-key ${ETHERSCAN_API_KEY} \
+--verify \
+--broadcast
